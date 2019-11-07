27a977f572eb58f3a5facebd9b3453a554cec265
Shourya Sengupta <sengupta.shourya@gmail.com>
2019-11-07 18:51:27 +0530

    removing scraper.py

--- news/scraper.py
+++ news/scraper.py
@@ -19,4 +19,3 @@ def scrape():
         image_source = [x.strip() for x in re.split(r' .[0-9]*[w][,]| .[0-9]*[w]', image_source)] # this splits the url and removes '1600w' from url fetched and removes whitespaces also
         image_source = image_source[:len(image_source)-1]
         image_source = image_source[1]
-scrape()
